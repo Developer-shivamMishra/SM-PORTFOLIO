28f2c4550ad908200e143a58dbbf5cf6ce0af256
Shivam <sm2275470@gmail.com>
2026-09-18 00:02:50 +0530

    feat: add main page component with routing and collapsible sidebar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -350,7 +350,7 @@ export default function Page() {
         </div>
 
         <div className="w-50 h-5 flex justify-end">
-          <p className="text-xs opacity-80 pr-2 pt-0.5">© Sk Akram</p>
+          <p className="text-xs opacity-80 pr-2 pt-0.5">© Shivam Mishra</p>
         </div>
       </div>
     </div>
